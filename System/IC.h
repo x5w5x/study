66df9ab66cd07f37b1289ef4de5d8b00e0db2929
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2025-07-20 21:51:38 +0800

    添加IC初始化及占空比获取功能

--- System/IC.h
+++ System/IC.h
@@ -1,6 +1,7 @@
 #ifndef _IC_H
 #define _IC_H
 void IC_Init(void);
+void IC_Init2(void);
 uint32_t IC_GetFreq(void);
-
+uint32_t IC_GetDuty(void);
 #endif
--- User/main.c
+++ User/main.c
@@ -22,16 +22,14 @@ int main(void)
 	Key_Init();
 	PWM_Init();
 	IC_Init();
-	OLED_ShowString(1,1,"Freq=");
+
 PWM_SetPrescaler(720-1);//Freq=72M/(PSC+1)/100
-PWM_SetCompare1(1);//Duty=CCR/100
+PWM_SetCompare1(2);//Duty=CCR/100
 
 
 
 
 	while(1){
-	OLED_ShowNum(1,6,IC_GetFreq(),6);
-
 
 	}
 	
