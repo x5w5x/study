171fc5630a3c3e294ed8216619c829ef4316eb61
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2025-07-28 22:18:04 +0800

    修复SPI初始化及数据交换，优化W25Q64读取ID显示

--- System/SPI.c
+++ System/SPI.c
@@ -5,74 +5,74 @@
 
 
 
-void SPI_W_SS(uint8_t bitValue)
-{
-    GPIO_WriteBit(GPIOA,GPIO_Pin_4,(BitAction)bitValue);
-}
+// void SPI_W_SS(uint8_t bitValue)
+// {
+//     GPIO_WriteBit(GPIOA,GPIO_Pin_4,(BitAction)bitValue);
+// }
 
-void SPI_W_SCK(uint8_t bitValue)
-{
-    GPIO_WriteBit(GPIOA,GPIO_Pin_5,(BitAction)bitValue);
-}
-
-
-void SPI_W_MOSI(uint8_t bitValue)
-{
-    GPIO_WriteBit(GPIOA,GPIO_Pin_7,(BitAction)bitValue);
-}
-
-uint8_t SPI_R_MISO(void)
-{
-    return GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_6);
-}
+// void SPI_W_SCK(uint8_t bitValue)
+// {
+//     GPIO_WriteBit(GPIOA,GPIO_Pin_5,(BitAction)bitValue);
+// }
 
 
+// void SPI_W_MOSI(uint8_t bitValue)
+// {
+//     GPIO_WriteBit(GPIOA,GPIO_Pin_7,(BitAction)bitValue);
+// }
+
+// uint8_t SPI_R_MISO(void)
+// {
+//     return GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_6);
+// }
 
 
-void SoftSPI_Init(void)
-{
-RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
-GPIO_InitTypeDef GPIO_InitStructure;
-GPIO_InitStructure.GPIO_Pin=GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_7;
-GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
-GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-GPIO_Init(GPIOA,&GPIO_InitStructure);
-GPIO_InitStructure.GPIO_Pin=GPIO_Pin_6;
-GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IPU;
-GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-GPIO_Init(GPIOA,&GPIO_InitStructure);
 
-SPI_W_SS(1);
-SPI_W_SCK(0);
-}
 
-void SPI_Start(void)
-{
-    SPI_W_SS(0);
-}
+// void SoftSPI_Init(void)
+// {
+// RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+// GPIO_InitTypeDef GPIO_InitStructure;
+// GPIO_InitStructure.GPIO_Pin=GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_7;
+// GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+// GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+// GPIO_Init(GPIOA,&GPIO_InitStructure);
+// GPIO_InitStructure.GPIO_Pin=GPIO_Pin_6;
+// GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IPU;
+// GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+// GPIO_Init(GPIOA,&GPIO_InitStructure);
 
-void SPI_Stop(void)
-{
-    SPI_W_SS(1);
-}
+// SPI_W_SS(1);
+// SPI_W_SCK(0);
+// }
 
-uint8_t SPI_SwapByte(uint8_t ByteSend)
-{
-    uint8_t i, ByteReceive=0x00;
-    for(i=0;i<8;i++)
-    {
-    SPI_W_MOSI(ByteSend&(0x80>>i));
-    SPI_W_SCK(1);
-    if(SPI_R_MISO())
-    {
-        ByteReceive|=(0x80>>i);
+// void SPI_Start(void)
+// {
+//     SPI_W_SS(0);
+// }
 
-    }
-    SPI_W_SCK(0);
-    }
-    return ByteReceive;
+// void SPI_Stop(void)
+// {
+//     SPI_W_SS(1);
+// }
+
+// uint8_t SPI_SwapByte(uint8_t ByteSend)
+// {
+//     uint8_t i, ByteReceive=0x00;
+//     for(i=0;i<8;i++)
+//     {
+//     SPI_W_MOSI(ByteSend&(0x80>>i));
+//     SPI_W_SCK(1);
+//     if(SPI_R_MISO())
+//     {
+//         ByteReceive|=(0x80>>i);
+
+//     }
+//     SPI_W_SCK(0);
+//     }
+//     return ByteReceive;
    
-}
+// }
 
 
 /*
@@ -101,3 +101,64 @@ uint8_t SPI_SwapByte(uint8_t ByteSend)
 
 
 
+
+
+
+void SPI_W_SS(uint8_t bitValue)
+{
+    GPIO_WriteBit(GPIOA,GPIO_Pin_4,(BitAction)bitValue);
+}
+
+
+
+
+void SoftSPI_Init(void)
+{
+RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1,ENABLE);
+GPIO_InitTypeDef GPIO_InitStructure;
+GPIO_InitStructure.GPIO_Pin=GPIO_Pin_4;
+GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+GPIO_Init(GPIOA,&GPIO_InitStructure);
+GPIO_InitStructure.GPIO_Pin=GPIO_Pin_5|GPIO_Pin_7;
+GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF_PP;
+GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+GPIO_Init(GPIOA,&GPIO_InitStructure);
+GPIO_InitStructure.GPIO_Pin=GPIO_Pin_6;
+GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IPU;
+GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+SPI_InitTypeDef SPI_InitStructure;
+SPI_InitStructure.SPI_Direction=SPI_Direction_2Lines_FullDuplex;  //方向全双工
+SPI_InitStructure.SPI_Mode=SPI_Mode_Master;//主机
+SPI_InitStructure.SPI_DataSize=SPI_DataSize_8b;//8位数据
+SPI_InitStructure.SPI_CPOL=SPI_CPOL_Low;//时钟极性
+SPI_InitStructure.SPI_CPHA=SPI_CPHA_1Edge;//时钟相位
+SPI_InitStructure.SPI_NSS=SPI_NSS_Soft;//NSS软件控制
+SPI_InitStructure.SPI_BaudRatePrescaler=SPI_BaudRatePrescaler_128;//波特率
+SPI_InitStructure.SPI_FirstBit=SPI_FirstBit_MSB;//高位先发
+SPI_InitStructure.SPI_CRCPolynomial=7;//CRC多项式
+SPI_Init(SPI1,&SPI_InitStructure);
+SPI_Cmd(SPI1,ENABLE);
+ SPI_W_SS(1);
+}
+
+void SPI_Start(void)
+{
+    SPI_W_SS(0);
+}
+
+void SPI_Stop(void)
+{
+    SPI_W_SS(1);    
+}
+
+uint8_t SPI_SwapByte(uint8_t ByteSend)
+{
+   while(SPI_I2S_GetFlagStatus(SPI1,SPI_I2S_FLAG_TXE)!=SET);
+    SPI_I2S_SendData(SPI1,ByteSend);
+    while(SPI_I2S_GetFlagStatus(SPI1,SPI_I2S_FLAG_RXNE)!=SET);
+    return SPI_I2S_ReceiveData(SPI1);
+}
--- User/main.c
+++ User/main.c
@@ -19,7 +19,7 @@
 #include"mpu6050.h"
 uint8_t MID;
 uint16_t DID;
-uint8_t wdata[]={0x55,0x66,0x77,0x88};
+uint8_t wdata[]={0x11,0x22,0x33,0x44};
 uint8_t rdata[4];
 int main(void)
 {
@@ -28,21 +28,10 @@ int main(void)
 	OLED_Init();
 	W25Q64_Init();
 
-	W25Q64_SectorErase(0x000000);
-	W25Q64_PageProgram(0x000000,wdata,4);
-	W25Q64_ReadData(0x000000,rdata,4);
-	
-	OLED_ShowHexNum(2,3,wdata[0],2);
-	OLED_ShowHexNum(2,6,wdata[1],2);
-	OLED_ShowHexNum(2,9,wdata[2],2);
-	OLED_ShowHexNum(2,12,wdata[3],2);
 
-	OLED_ShowHexNum(4,3,rdata[0],2);
-	OLED_ShowHexNum(4,6,rdata[1],2);
-	OLED_ShowHexNum(4,9,rdata[2],2);
-	OLED_ShowHexNum(4,12,rdata[3],2);
-
-	// OLED_ShowHexNum(1,1,MPU6050_GetID(),3);
+W25Q64_ReadID(&MID,&DID);
+	OLED_ShowHexNum(1,1,MID,3);
+	OLED_ShowHexNum(1,5,DID,4);
 
 
 		
