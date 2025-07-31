4c93fe22172fd6f9b8866af7910ff5d882a1be79
慕芸婷 <12344332+xx3457388139@user.noreply.gitee.com>
2025-07-31 21:43:37 +0800

    添加WWDG初始化及按键喂狗功能

--- User/main.c
+++ User/main.c
@@ -2,7 +2,7 @@
 #include "stm32f10x.h" 
 #include "Delay.h"
 // #include "led.h"
-// #include "Key.h"
+#include "Key.h"
 // #include"Buzzer.h"
 // #include"LightSensor.h"
 #include "OLED.h"
@@ -25,21 +25,42 @@ int main(void)
 {
 	
 
-	OLED_Init();
-	RTC_Init();
+	OLED_Init();//初始化OLED
+	Key_Init();//初始化按键
+	OLED_ShowString(1,1,"WWDG TEST");
+	if(RCC_GetFlagStatus(RCC_FLAG_WWDGRST)==SET)
+	{
+		OLED_ShowString(2,1,"WWDGRST");
+		Delay_ms(500);
+		OLED_ShowString(2,1,"        ");
+		Delay_ms(100);
+	    RCC_ClearFlag();
+	}
+	else
+	{
+	    OLED_ShowString(3,1,"RST");
+		Delay_ms(500);
+		OLED_ShowString(3,1,"        ");
+		Delay_ms(100);
+	}
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_WWDG,ENABLE);//使能WWDG时钟
+	WWDG_SetPrescaler(WWDG_Prescaler_8);//设置WWDG分频系数
+	WWDG_SetWindowValue(0x40 | 21);//设置WWDG窗口值
+	WWDG_Enable(0x40 | 54);//使能WWDG
 
-OLED_ShowString(1,1,"CNT:");
-OLED_ShowString(2,1,"ALR:");
-OLED_ShowString(3,1,"ALRF:");
+
+	while(1){
+		Key_GetNum();
+		
+	
+		OLED_ShowString(4,1,"FEED");
+		Delay_ms(20);
+		OLED_ShowString(4,1,"     ");
+		Delay_ms(15);
+		WWDG_SetCounter(0x40 | 54);//喂狗
+}}
 
 
 
 	
 
-
-		
-	while(1){
-		OLED_ShowNum(4,1,RTC_GetCounter(),10);
-
-
-}}
